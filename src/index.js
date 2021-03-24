5c2eece2f6c144b166a7eb60e79c933941f5b021
yukaoi <aoyk.ooiooi@gmail.com>
2021-03-25 01:18:26 +0900

    STOREのやりかた（まどぐちを作る）

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+//reducerをもとに作成
+//すべての場所でSTOREが使えるようにする
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';//元々用意されている
+import {Provider} from 'redux';//特殊コンポーネント、作成したSTOREを全コーポネントに渡すため
 
+import './index.css';
+import reducer from ',./reducers'//前回作成したもの
+import App from './components/App';//見通ししやすいよにディレクトリをつくってここにいれておく
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer)//アプリ内で唯一のもの　全てのstateはここに集約されている
+
+//集約されているとリレー形式で渡す必要がなくなった
+//providerをつかってAPPコンポーネントをラップしてSTORE属性にSTOREをわたす。これだけでOK
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
+registerServiceWorker();
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
