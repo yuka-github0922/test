f4cb51323ecb99f19e06de5ad8843a0a8a6ee91f
yukaoi <aoyk.ooiooi@gmail.com>
2021-03-24 23:54:49 +0900

    stateの練習（書き換え方)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-//prop typesについて→プロパティについての型チェックについて（TSみたいな、もん）
-import propTypes　from 'prop-types';
+//STATEについてPROPSとのちがい
+// コンポーネントの内部だけで使用される。変更可能な点が違い
+import React,　{ Component }　from 'react';
 
-const App = () =>{
-  const profiles = [
-    {name: "hana", age: 10},
-    {name: "Hanako", age: 5},
-    {name: "きれいなobasan", age: 40},
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index)=>{
-          return (
-            <User 
-            name = {profile.name}
-            age = {profile.age}
-            key = {index}
-            />
-          )
-        })
-      }
-    </div>
+const App = () => (<Counter></Counter>)//ここでcounterのインスタンスが呼び出される
+
+class Counter extends Component {
+  //componentの初期化時にはconstructorが必要
+  constructor(props){
+    super(props)
+    console.log(this.state)
+    this.state = {count : 0}//countの値を設定（初期設定）
+  }
+
+  handlePlusButton = () =>{
+    console.log("handlePlusButton")
+    this.setState({ count: this.state.count + 1})//状態変更したいときにおきまりのやつ[setState]必須
+    //setState呼ぶとrenderがコールバックとして呼び出される
+  }
+  handleMinusButton = () =>{
+    this.setState({ count: this.state.count - 1})//これES６の課題でできそう
+  }
+  
+  render() {
+    return(
+    <React.Fragment>
+     <div>count: {this.state.count}</div>
+      <button onClick = {this.handlePlusButton}>+1</button>
+      <button onClick = {this.handleMinusButton}>-1</button>
+    </React.Fragment>
   )
-}
-const User = (props) => {
-  return <div>Hi!myName is {props.name}私は{props.age}歳です</div>
-}
-User.propTypes = {
-  name: propTypes.string,
-  age: propTypes.number.isRequired//isRequiredでageは絶対に入力されていないといけないデータになる
+  }
 }
 
 export default App;
