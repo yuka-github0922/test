3ea6eeb46f70d646ec7e28a0d75c9a356027a16f
yukaoi <aoyk.ooiooi@gmail.com>
2021-03-24 23:14:41 +0900

    propTypesの練習（型定義)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Profiler } from 'react';
+//prop typesについて→プロパティについての型チェックについて（TSみたいな、もん）
+import propTypes　from 'prop-types';
 
 const App = () =>{
   const profiles = [
-    {name: "Taro", age: 10},
+    {name: "hana", age: 10},
     {name: "Hanako", age: 5},
-    {name: "きれいなobasan"}
+    {name: "きれいなobasan", age: 40},
   ]
   return (
     <div>
@@ -14,7 +15,7 @@ const App = () =>{
             <User 
             name = {profile.name}
             age = {profile.age}
-            key = {index}//consoleでのwarningに対する処置
+            key = {index}
             />
           )
         })
@@ -25,8 +26,9 @@ const App = () =>{
 const User = (props) => {
   return <div>Hi!myName is {props.name}私は{props.age}歳です</div>
 }
-User.defaultProps = {//defaultを入れることで、もし年齢がない場合もここに設定した値を入れることができる
-  age: 1
+User.propTypes = {
+  name: propTypes.string,
+  age: propTypes.number.isRequired//isRequiredでageは絶対に入力されていないといけないデータになる
 }
 
 export default App;
